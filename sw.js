a5f4aeaa7221db4a37bc64aa5088feffd755fb0b
Lorenzo Jaime Castro Vazquez <lorenzojaimecastrovazquez@MBPROJCV.local>
2021-05-01 18:43:01 -0500

    primer commit

---
+++ sw.js
@@ -0,0 +1,72 @@
+// imports declara los scripts que tienen otras funciones que usamos en este archivo de js
+
+importScripts('js/sw-utils.js');
+
+const STATIC_CACHE    = 'static-v2';
+const DYNAMIC_CACHE   = 'dynamic-v1';
+const INMUTABLE_CACHE = 'inmutable-v1';
+
+const  APP_SHELL_STATIC = [  // Aqui esta el corazon de la App y lo que debe cargarse los mas rápido posible
+    '/',
+    'index.html',
+    'css/style.css',
+    'img/favicon.ico',
+    'img/avatars/spiderman.jpg',
+    'img/avatars/hulk.jpg',
+    'img/avatars/ironman.jpg',
+    'img/avatars/thor.jpg',
+    'img/avatars/wolverine.jpg',
+    'js/app.js',
+    'js/sw-utils.js'
+];
+
+const APP_SHELL_INMUTABLE = [ // Aqui va todo lo que nunca va a cambiar en la App
+    'https://fonts.googleapis.com/css?family=Quicksand:300,400',
+    'https://fonts.googleapis.com/css?family=Lato:400,300',
+    "https://use.fontawesome.com/releases/v5.3.1/css/all.css",
+    "css/animate.css",
+    "js/libs/jquery.js"
+];
+
+self.addEventListener('install', e => {
+
+    const cacheStatic = caches.open(STATIC_CACHE).then(cache => 
+        cache.addAll(APP_SHELL_STATIC));
+
+    const cacheInmutable = caches.open(INMUTABLE_CACHE).then(cache => 
+        cache.addAll(APP_SHELL_INMUTABLE));
+
+    e.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
+
+});
+
+self.addEventListener('activate', e => { // Aqui se activa el SW y vamos a usarlo para borrar caches viejos y asi evitar llenar la memoria del dispositivo
+
+
+    const respuesta = caches.keys().then(keys => { // En estos keys esta en nombre de los caches
+        keys.forEach(key => {
+            if (key !== STATIC_CACHE && key.includes('static')) {
+                return caches.delete(key);
+            }
+        });
+    });
+
+    e.waitUntil(respuesta);
+
+});
+
+self.addEventListener('fetch', e => {
+
+    const   respuesta = caches.match(e.request).then(res => {
+            if (res) {
+                return res;
+            } else {
+                return fetch(e.request).then( newRes => {
+                    return actualizaCacheDinamico(DYNAMIC_CACHE, e.request, newRes);
+                });
+            }
+    });
+
+    e.respondWith(respuesta);
+});
+
