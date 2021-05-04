6c7f745216be91ee59abbab5b42b574df1d8aacf
Jaime Castro <jaime.castro5075@gmail.com>
2021-05-03 23:32:37 -0500

    modificaciones de iphone x+

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@
 
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL_STATIC = [ // Aqui esta el corazon de la App y lo que debe cargarse los mas rápido posible
@@ -46,6 +46,9 @@ self.addEventListener('activate', e => { // Aqui se activa el SW y vamos a usarl
     const respuesta = caches.keys().then(keys => { // En estos keys esta en nombre de los caches
         keys.forEach(key => {
             if (key !== STATIC_CACHE && key.includes('static')) {
+                return caches.delete(key);
+            }
+            if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
                 return caches.delete(key);
             }
         });
